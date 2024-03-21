3a9214ea3f1288f8b073d499fe1e9b6d979e3edb
Julio S. Castro <juyocastr@gmail.com>
2024-03-20 23:11:32 -0300

    linter

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import styles from './Button.module.css'
+import React from 'react';
+import styles from './Button.module.css';
 
-const Button = ({children, ...props}) => {
+const Button = ({ children, ...props }) => {
   return (
-    <button {...props} className={styles.button}>{children}</button>
-  )
-}
+    <button {...props} className={styles.button}>
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,49 +1,51 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Input from '../Input'
-import Button from '../Button'
-import useForm from '../../Hooks/useForm'
-import { UserContext } from '../../userContext'
-import Error from '../Helper/Error'
-import styles from './LoginForm.module.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Input from '../Input';
+import Button from '../Button';
+import useForm from '../../Hooks/useForm';
+import { UserContext } from '../../userContext';
+import Error from '../Helper/Error';
+import styles from './LoginForm.module.css';
 
 const LoginForm = () => {
-  const username = useForm()
-  const password = useForm()
+  const username = useForm();
+  const password = useForm();
 
-  const { userLogin, error, loading } = React.useContext(UserContext)
+  const { userLogin, error, loading } = React.useContext(UserContext);
 
   async function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    if(username.validate() && password.validate()) {
-      userLogin(username.value, password.value)
+    if (username.validate() && password.validate()) {
+      userLogin(username.value, password.value);
     }
-  }  
+  }
 
   return (
-    <section className='animeLeft'>
-      <h1 className='title'>Login</h1>
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
       <form action="" onSubmit={handleSubmit} className={styles.form}>
-        <Input label='Usuário' type='text' name='username' {...username} />
-        <Input label='Senha' type='password' name='password' {...password} />
-        { loading ? (
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        {loading ? (
           <Button disabled>Carregando...</Button>
         ) : (
           <Button>Entrar</Button>
         )}
-        <Error error={error}/>
+        <Error error={error} />
       </form>
-      <Link className={styles.perdeu} to='/login/perdeu'>
+      <Link className={styles.perdeu} to="/login/perdeu">
         Perdeu a senha?
       </Link>
       <div className={styles.cadastro}>
         <h2 className={styles.subtitle}>Cadastre-se</h2>
         <p>Ainda não possui conta? Cadastre-se no site.</p>
-        <Link className={styles.buttonCriar} to='/login/criar'>Criar conta</Link>
+        <Link className={styles.buttonCriar} to="/login/criar">
+          Criar conta
+        </Link>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
