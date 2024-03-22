0f7f0a68b0d3af8b0d242cfeed3bc17ac621aba3
Julio S. Castro <juyocastr@gmail.com>
2024-03-21 22:09:08 -0300

    0818-mobile-menu-1200

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -6,11 +6,13 @@ import Estatisticas from '../../Assets/estatisticas.svg?react';
 import AdicionarFoto from '../../Assets/adicionar.svg?react';
 import Sair from '../../Assets/sair.svg?react';
 import styles from './UserHeaderNav.module.css';
+import useMedia from '../../Hooks/useMedia';
 
 const UserHeaderNav = () => {
   const { userLogout } = React.useContext(UserContext);
-  const [mobile, setMobile] = React.useState(null);
   const navigate = useNavigate();
+  const mobile = useMedia('(max-width:40rem)');
+  const [mobileMenu, setMobileMenu] = React.useState(false);
 
   function handleLogout() {
     userLogout();
@@ -18,20 +20,28 @@ const UserHeaderNav = () => {
   }
 
   return (
-    <nav className={styles.nav}>
-      <NavLink to="/conta" end>
-        <MinhasFotos /> {mobile && 'Minhas fotos'}
-      </NavLink>
-      <NavLink to="/conta/estatisticas">
-        <Estatisticas /> {mobile && 'Estatísticas'}
-      </NavLink>
-      <NavLink to="/conta/postar">
-        <AdicionarFoto /> {mobile && 'Adicionar foto'}
-      </NavLink>
-      <button onClick={handleLogout}>
-        <Sair /> {mobile && 'Sair'}
-      </button>
-    </nav>
+    <>
+      <button
+        aria-label="Menu"
+        className={`${styles.mobileButton}
+          ${mobileMenu && styles.mobileButtonActive}`}
+        onClick={() => setMobileMenu(!mobileMenu)}
+      ></button>
+      <nav className={styles.nav}>
+        <NavLink to="/conta" end>
+          <MinhasFotos /> {mobile && 'Minhas fotos'}
+        </NavLink>
+        <NavLink to="/conta/estatisticas">
+          <Estatisticas /> {mobile && 'Estatísticas'}
+        </NavLink>
+        <NavLink to="/conta/postar">
+          <AdicionarFoto /> {mobile && 'Adicionar foto'}
+        </NavLink>
+        <button onClick={handleLogout}>
+          <Sair /> {mobile && 'Sair'}
+        </button>
+      </nav>
+    </>
   );
 };
 
